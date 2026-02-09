3ca82fbfebfd71900f7f5a4609473bc3905eb6bf
LaysArceles <LaysArceles12@gmail.com>
2026-02-09 10:32:02 -0300

    Rotas

--- Api's/src/server.ts
+++ Api's/src/server.ts
@@ -1,27 +1,28 @@
 import express from 'express'
+import person from ''
 
 const port = 8080;
 const app = express();
 
-const people = {
-    name: "Lays",
-    lestname: "Arceles"
-}
-// Jeito Certo
-app.get('/objeto',(req,res)=>{
-    // nome que ira chegar 
-    res.send({people: people})
-})
+// const people = {
+//     name: "Lays",
+//     lestname: "Arceles"
+// }
+// // Jeito Certo
+// app.get('/objeto',(req,res)=>{
+//     // nome que ira chegar 
+//     res.send({people: people})
+// })
 
-app.get('/direto',(req,res)=>{
-    res.send({people})
-})
+// app.get('/direto',(req,res)=>{
+//     res.send({people})
+// })
 
-//consulta rotas
-//http://localhost:8080/
-app.get('/',(req,res)=>{
-    res.send({response: "Api funcional!"})
-})
+// //consulta rotas
+// //http://localhost:8080/
+// app.get('/',(req,res)=>{
+//     res.send({response: "Api funcional!"})
+// })
 
 app.listen(port,() => {
     console.log(`Servidor rodando na porta ${port}`)
