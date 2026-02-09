b039cb2d7e43e425769e6e197f94403f14ec320d
LaysArceles <LaysArceles12@gmail.com>
2026-02-09 09:27:00 -0300

    Objeto sendo chamado

--- Api's/src/server.ts
+++ Api's/src/server.ts
@@ -3,6 +3,20 @@ import express from 'express'
 const port = 8080;
 const app = express();
 
+const people = {
+    name: "Lays",
+    lestname: "Arceles"
+}
+// Jeito Certo
+app.get('/objeto',(req,res)=>{
+    // nome que ira chegar 
+    res.send({people: people})
+})
+
+app.get('/direto',(req,res)=>{
+    res.send({people})
+})
+
 //consulta rotas
 //http://localhost:8080/
 app.get('/',(req,res)=>{
