024176d3288c7ebfa7ba4eb277e2cafcbe6f9218
LaysArceles <LaysArceles12@gmail.com>
2026-02-09 11:36:57 -0300

    push

--- Api's/src/routes/person.ts
+++ Api's/src/routes/person.ts
@@ -6,9 +6,12 @@ const people: object[] = [];
 router
      .post('/add', (req:Request,res:Response) => {
         const{name,lastname, age, image}= req.body
-        
-        console.log (name,lastname, age, image)
-
+        // console.log (name,lastname, age, image)
+        people.push({name,lastname, age, image})
+        res.status(200).send({message:'Usario ${name} %{lastname} cadastrado com sucesso!'})
+     })
+     .get ('/usuario',(req:Request,res:Response) => {
+        res.status(200).send({users:people})
      })
 //....
 
