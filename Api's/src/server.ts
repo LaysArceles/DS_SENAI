b09312e161ded1bfe7be425ec5fbebc4eef8ec9d
LaysArceles <LaysArceles12@gmail.com>
2026-02-09 09:16:05 -0300

    rotas

--- Api's/src/server.ts
+++ Api's/src/server.ts
@@ -3,6 +3,12 @@ import express from 'express'
 const port = 8080;
 const app = express();
 
+//consulta rotas
+//http://localhost:8080/
+app.get('/',(req,res)=>{
+    res.send({response: "Api funcional!"})
+})
+
 app.listen(port,() => {
     console.log(`Servidor rodando na porta ${port}`)
 })
